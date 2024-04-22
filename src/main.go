2243432ee8802d5b198bdf98c079b87c5b650fd2
timoruohomaki <timo@ruohomaki.fi>
2024-04-22 09:24:12 +0300

    initial API service implementation using net/http

--- src/main.go
+++ src/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-	"github.com/timoruohomaki/open311togo/telemetrics"
+	"net/http"
+	"fmt"
+
 	"github.com/timoruohomaki/open311togo/storage"
+	"github.com/timoruohomaki/open311togo/telemetrics"
 )
 
 func main() {
@@ -15,5 +18,14 @@ func main() {
 
 	storage.MongoGetDatabases()
 
+	// start api service
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/path/to/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		fmt.Fprint(w, "Thank you for calling the path number ",id)
+	})
+
+	http.ListenAndServe("localhost:8080", mux)
 	
 }
---
+++ src/types/structures.go
@@ -0,0 +1,60 @@
+package types
+
+import "sync"
+
+type SpatialGeometry struct {
+	id int
+	geometryType string
+	coordinates []double
+}
+
+type SpatialFeature struct {
+	id int
+	authorityName string
+	authorityResourceURI string
+	featureType string
+	featureGeometry SpatialGeometry
+
+}
+
+type CustomFeatureProperties struct {
+	id int
+}
+
+// as in https://docs.ogc.org/is/18-088/18-088.html#featureofinterest
+
+type FeatureOfInterest struct {
+	id int								`json:"id"`
+	name string							`json:"name"`
+	description string					`json:"description"`
+	featureEncodingType string			`json:"featureEncodinType"`
+	feature SpatialFeature				`json:"feature"`
+	properties CustomFeatureProperties	`json:"properties"`
+}
+
+type ServiceDefinition struct {
+	id int
+}
+
+type ServiceRequest struct {
+	id int
+}
+
+type Defaults struct {
+	mongoServiceCollection string
+	mongoRequestCollection string
+}
+
+type ServiceStore struct {
+	sync.Mutex
+
+	services map[int]ServiceDefinition
+	nextId int
+}
+
+type RequestStore struct {
+	sync.Mutex
+
+	requests map[int]ServiceRequest
+
+}
