f3c150c46554db3f12bc15aeafd232638d0c5a01
Timo Ruohomaki <timo@ruohomaki.fi>
2024-04-28 22:52:46 +0300

    test commit

--- src/models/structures.go
+++ src/models/structures.go
@@ -2,25 +2,22 @@ package models
 
 import (
 	"sync"
-
-	"go.mongodb.org/mongo-driver/mongo/description"
 )
 
 type SpatialGeometry struct {
 	// json as in GeoJSON (RFC 7946)
-	
-	id int `json:"id"`
-	geometryType string `json:"type"`
-	coordinates []float64 `json:"coordinates"`
+
+	id           int       `json:"id"`
+	geometryType string    `json:"type"`
+	coordinates  []float64 `json:"coordinates"`
 }
 
 type SpatialFeature struct {
-	id int
-	authorityName string
+	id                   int
+	authorityName        string
 	authorityResourceURI string
-	featureType string
-	featureGeometry SpatialGeometry
-
+	featureType          string
+	featureGeometry      SpatialGeometry
 }
 
 type CustomFeatureProperties struct {
@@ -28,36 +25,36 @@ type CustomFeatureProperties struct {
 }
 
 type KeyName struct {
-	key 	string
-	name 	string
+	key  string
+	name string
 }
 
 type ServiceDefinitionAttribute struct {
-	variable	string
-	code 	string
-	datatype 	string
-	required	bool
-	datatype_description	string
-	order	int
-	description	string
-	values	[]KeyName
+	variable             string
+	code                 string
+	datatype             string
+	required             bool
+	datatype_description string
+	order                int
+	description          string
+	values               []KeyName
 }
 
 // as in https://docs.ogc.org/is/18-088/18-088.html#featureofinterest
 
 type FeatureOfInterest struct {
-	id int			`json:"id"`
-	name string				`json:"name"`
-	description string			`json:"description"`
-	featureEncodingType string		`json:"featureEncodinType"`
-	feature SpatialFeature		`json:"feature"`
-	properties CustomFeatureProperties	`json:"properties"`
+	id                  int                     `json:"id"`
+	name                string                  `json:"name"`
+	description         string                  `json:"description"`
+	featureEncodingType string                  `json:"featureEncodinType"`
+	feature             SpatialFeature          `json:"feature"`
+	properties          CustomFeatureProperties `json:"properties"`
 }
 
 type ServiceDefinition struct {
 	service_code string
-	ptv_code_URI 	string
-	attribute 	ServiceDefinitionAttribute
+	ptv_code_URI string
+	attribute    ServiceDefinitionAttribute
 }
 
 type ServiceRequest struct {
@@ -73,12 +70,11 @@ type ServiceStore struct {
 	sync.Mutex
 
 	services map[int]ServiceDefinition
-	nextId int
+	nextId   int
 }
 
 type RequestStore struct {
 	sync.Mutex
 
 	requests map[int]ServiceRequest
-
 }
