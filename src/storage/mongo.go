9ea665bb8e6d3c8df2dc4a3653deb5e34fb7e10b
timoruohomaki <timo@ruohomaki.fi>
2024-07-07 10:55:42 +0300

    updates

--- src/storage/mongo.go
+++ src/storage/mongo.go
@@ -10,7 +10,7 @@ import (
 	"github.com/timoruohomaki/open311togo/telemetry"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
- "go.mongodb.org/mongo-driver/bson/primitive"
+ 	"go.mongodb.org/mongo-driver/bson/primitive"
 	//"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
